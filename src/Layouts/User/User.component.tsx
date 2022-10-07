a6174754cae93cd6b78251cf50dab28df2ef8a7c
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-07 03:14:03 +0100

    completed navbar

--- src/Layouts/User/User.component.tsx
+++ src/Layouts/User/User.component.tsx
@@ -1,28 +1,67 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useLocalStorage } from 'Hooks/useLocalStorage';
 import { ReactComponent as CaretDownIcon } from 'assets/svg/caret-down.svg';
+import { useNavigate } from 'react-router-dom';
 import './User.styles.scss';
 
 const User = () => {
   const [open, setOpen] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+  const navigate = useNavigate();
 
   const toggleDropdown = () => {
     setOpen((prevS) => !prevS);
+
+    setTimeout(() => {
+      ref.current && ref.current.focus();
+    }, 100);
   };
-  const { getLocalStorage } = useLocalStorage('user');
+
+  const { getLocalStorage, removeLocalStorage } = useLocalStorage('user');
+
+  const handleLogout = () => {
+    removeLocalStorage();
+    navigate('auth/sign-in');
+  };
+
   return (
     <div className='user-dropdown'>
-      <img
-        src='/img/user.webp'
-        alt='user avatar'
-        className='user-dropdown-avatar'
-      />
-      <p className='user-dropdown-username' onClick={toggleDropdown}>
-        {getLocalStorage?.email.slice(0, getLocalStorage?.email.indexOf('@'))}
-      </p>
-      <CaretDownIcon className='user-dropdown-caret' />
+      <div className='user-dropdown-container' onClick={toggleDropdown}>
+        <img
+          src='/img/user.webp'
+          alt='user avatar'
+          className='user-dropdown-container-avatar'
+        />
+        <p className='user-dropdown-container-username'>
+          {getLocalStorage?.email.slice(0, getLocalStorage?.email.indexOf('@'))}
+        </p>
+        <CaretDownIcon className='user-dropdown-container-caret' />
+      </div>
 
-      {open && <div className='user-dropdown-dropdown'></div>}
+      {open && (
+        <div
+          ref={ref}
+          tabIndex={1}
+          onBlur={toggleDropdown}
+          className='user-dropdown-dropdown'
+        >
+          <p className='user-dropdown-dropdown-email'>
+            {getLocalStorage?.email}
+          </p>
+          <p className='user-dropdown-dropdown-profile user-dropdown-dropdown-item'>
+            Profile
+          </p>
+          <p className='user-dropdown-dropdown-setting user-dropdown-dropdown-item'>
+            Settings
+          </p>
+          <p
+            className='user-dropdown-dropdown-logout user-dropdown-dropdown-item'
+            onClick={handleLogout}
+          >
+            Logout
+          </p>
+        </div>
+      )}
     </div>
   );
 };
