6cffc05d9b0bd5cf6e1b63bfb4e99cefa77e3912
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-10 06:11:58 +0100

    modified SwitchOrganisation component to use useClickOutside hook

--- src/Layouts/Sidebar/SwitchOrganisation/SwitchOrganisation.component.tsx
+++ src/Layouts/Sidebar/SwitchOrganisation/SwitchOrganisation.component.tsx
@@ -1,22 +1,18 @@
-import { useState, useRef } from 'react';
+import * as React from 'react';
 import { ReactComponent as OrganizationIcon } from 'assets/svg/briefcase.svg';
 import { ReactComponent as ChevronIcon } from 'assets/svg/chevron-down.svg';
+import { useClickOutside } from 'Hooks/useClickOutside';
 import './SwitchOrganisation.styles.scss';
 
-const SwitchOrganisation = () => {
-  const [open, setOpen] = useState(false);
-  const ref = useRef<HTMLDivElement>(null);
+const SwitchOrganisation: React.FC = () => {
+  const { visible, setVisible, ref, ref1 } = useClickOutside();
 
   const toggleDropdown = () => {
-    setOpen((prevS) => !prevS);
-
-    setTimeout(() => {
-      ref.current && ref.current.focus();
-    }, 100);
+    setVisible((prevS) => !prevS);
   };
 
   return (
-    <div className='switch-organisation'>
+    <div className='switch-organisation' ref={ref1}>
       <div className='switch-organisation-contents' onClick={toggleDropdown}>
         <OrganizationIcon />
         <p className='switch-organisation-contents-text'>Switch Organization</p>
@@ -24,18 +20,13 @@ const SwitchOrganisation = () => {
       </div>
 
       {/* dropdown */}
-      {open && (
-        <div
-          tabIndex={-1}
-          ref={ref}
-          onBlur={() => setOpen(false)}
-          className='switch-organisation-drop-down'
-        >
+      {visible && (
+        <div ref={ref} className='switch-organisation-drop-down'>
           {['LendSqr', 'FastPay', 'Borrow Me', 'Pay Later'].map((org) => (
             <p
               key={org}
               className='switch-organisation-drop-down-items'
-              onClick={() => setOpen(false)}
+              onClick={toggleDropdown}
             >
               {org}
             </p>
