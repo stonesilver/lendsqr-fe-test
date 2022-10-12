4621b6e0c68b21f17ee1710408dfb7e9c292b2f0
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-12 04:44:30 +0100

    added functionality to close sidebar when nav link is clicked

--- src/Layouts/Sidebar/NavLink/NavLink.component.tsx
+++ src/Layouts/Sidebar/NavLink/NavLink.component.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import TableContext from 'Context/TableContext';
 import './NavLink.styles.scss';
 
 interface Props {
@@ -8,10 +9,13 @@ interface Props {
 }
 
 const NavLink: React.FC<Props> = ({ svgSrc, text }) => {
+  const { toggleHamburger } = React.useContext(TableContext);
+
   return (
     <Link
       to={text.toLocaleLowerCase().replaceAll(' ', '-')}
       className='side-nav-link'
+      onClick={toggleHamburger}
     >
       <img src={svgSrc} alt={text} className='side-nav-link-icon' />
       <p className='side-nav-link-text'>{text}</p>
