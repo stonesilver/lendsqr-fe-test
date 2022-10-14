38bcec2538d4c87e3e6ee62b021438593953595e
ezenwa chigozie <stonesilver12@gmail.com>
2022-10-14 13:40:44 +0100

    fixed hamburger menu onClick

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { TableProvider } from 'Context/TableContext';
 import './index.scss';
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,10 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <TableProvider>
+
           <App />
+    </TableProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/Users/Users.page.tsx
+++ src/pages/Users/Users.page.tsx
@@ -1,17 +1,14 @@
 import UsersTable from './UsersTable/UsersTable.component';
 import UserStatistics from './UserStatistics/UserStatistics.component';
-import { TableProvider } from 'Context/TableContext';
 import './Users.styles.scss';
 
 const Users = () => {
   return (
-    <TableProvider>
       <div className='users-page'>
         <header className='users-page-header'>Users</header>
         <UserStatistics />
         <UsersTable />
       </div>
-    </TableProvider>
   );
 };
 
